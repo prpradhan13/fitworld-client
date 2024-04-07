d59f9d9dff85057698568c8e9587dd15de4d3c74
Pritiranjan Pradhan <ppritiranjan7@gmail.com>
2024-04-07 22:19:41 +0530

    page error fix

--- src/components/FallbackComp.jsx
+++ src/components/FallbackComp.jsx
@@ -3,7 +3,7 @@
 function FallbackComp() {
   return (
     <div className="w-full h-[80vh] px-3 md:px-9 lg:px-24 py-3 md:py-6 flex justify-center items-center">
-      <h1 className="text-6xl font-bold text-white">Please Wait...</h1>
+      <h1 className="text-3xl font-bold text-white">Please Wait...</h1>
     </div>
   )
 }
